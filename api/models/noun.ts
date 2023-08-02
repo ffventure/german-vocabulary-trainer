1e31ed965eaa3be3ec026ebfdc7c89cfc51ac166
ffventure <137869989+ffventure@users.noreply.github.com>
2023-08-02 22:52:21 +0200

    Remodel noun

--- api/models/noun.ts
+++ api/models/noun.ts
@@ -6,37 +6,46 @@ enum GenusEnum {
   Neutrum = 'n'
 }
 
-interface Declension {
-  nominative: string;
-  genitive: string;
-  dative: string;
-  accusative: string;
-}
-
 interface Noun {
   word: string;
   genus: string;
-  singular: Declension;
-  plural: Declension;
-  sk: string;
+  singular: {
+    nominative: string;
+    genitive: string;
+    dative: string;
+    accusative: string;
+  };
+  plural: {
+    nominative: string;
+    genitive: string;
+    dative: string;
+    accusative: string;
+  };
+  translations: [{
+    language: string;
+    translation: string;
+  }];
 }
 
 const nounSchema = new Schema<Noun>({
   word: { type: String, required: true },
   genus: { type: String, required: true, enum: GenusEnum },
-  singular: new Schema<Declension>({
+  singular: {
     nominative: { type: String, required: true },
     genitive: { type: String, required: true },
     dative: { type: String, required: true },
     accusative: { type: String, required: true },
-  }),
-  plural: new Schema<Declension>({
+  },
+  plural: {
     nominative: { type: String, required: true },
     genitive: { type: String, required: true },
     dative: { type: String, required: true },
     accusative: { type: String, required: true },
-  }),
-  sk: { type: String, required: true }
+  },
+  translations: [{
+    language: {type: String, required: true},
+    translation: {type: String, required: true}
+  }]
 })
 
 const Noun = model<Noun>('Noun', nounSchema)
